df991aa784f5fb63a19a736b26ce8d32cddfc0ff
Narciso Gomes <narcisoalmeida25@gmail.com>
2022-03-31 21:46:52 -0300

    show case

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
