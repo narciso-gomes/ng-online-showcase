bd665a7df8f3ebfa7ed0511255aebc7fd984d913
Narciso Gomes <narcisoalmeida25@gmail.com>
2022-04-01 08:39:35 -0300

    ADD: infinite scroll

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  public products = [1, 2];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    if (this.bottomReached()) {
+      this.products = [...this.products, 1, 2, 3];
+    }
+  }
+
+  private bottomReached(): boolean {
+    return window.innerHeight + window.scrollY >= document.body.offsetHeight;
+  }
 }
+
